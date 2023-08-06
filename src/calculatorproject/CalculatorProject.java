76f108d60db2209d284951d21731a97a28fa5ef7
macbook <macbook@218.38.137.28>
2023-08-06 15:26:43 +0900

    define the buttons

--- src/calculatorproject/CalculatorProject.java
+++ src/calculatorproject/CalculatorProject.java
@@ -6,8 +6,34 @@ import javax.swing.*;
 public class CalculatorProject {
     
     JFrame frame = new JFrame("Calculator");
+    JLabel label = new JLabel();
+    JTextField textField = new JTextField();
+    JRadioButton onRadioButton = new JRadioButton("on");
+    JRadioButton offRadioButton = new JRadioButton ("off");
+    JButton buttonZero = new JButton("0");
+    JButton buttonOne = new JButton("1");
+    JButton buttonTwo = new JButton("2");
+    JButton buttonThree = new JButton("3");
+    JButton buttonFour = new JButton("4");
+    JButton buttonFive = new JButton("5");
+    JButton buttonSix = new JButton("6");
+    JButton buttonSeven = new JButton("7");
+    JButton buttonEight = new JButton("8");
+    JButton buttonNine = new JButton("9");
+    JButton buttonDot = new JButton(".");
+    JButton buttonClear = new JButton("C");
+    JButton buttonDelete = new JButton("DEL");
+    JButton buttonEqual = new JButton("=");
+    JButton buttonMul = new JButton("X");
+    JButton buttonDiv = new JButton("/");
+    JButton buttonPlus = new JButton("+");
+    JButton buttonMinus = new JButton("-");
+    JButton buttonSquare = new JButton("x\u00B2");
+    JButton buttonReciprocal = new JButton("1/x");
+    JButton buttonSqrt = new JButton("\u221A");
     
     CalculatorProject(){
+        prepareGui();
     
     }
     
