378b536b38b10ddfc8bfb120ce7e553d8f0f1875
macbook <macbook@218.38.137.28>
2023-08-06 14:50:57 +0900

    import swing

--- src/calculatorproject/CalculatorProject.java
+++ src/calculatorproject/CalculatorProject.java
@@ -1,11 +1,19 @@
 
 package calculatorproject;
 
-
+import javax.swing.*;
 public class CalculatorProject {
+    
+    JFrame frame = new JFrame("Calculator");
+    
+    CalculatorProject(){
+    
+    }
 
    
     public static void main(String[] args) {
+        
+        CalculatorProject calculator = new CalculatorProject();
         
     }
     
