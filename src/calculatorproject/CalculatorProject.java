6be04a6b589b09b496739c293a57f0edf81a1ee4
macbook <macbook@218.38.137.28>
2023-08-06 15:03:43 +0900

    define a user define method for screen

--- src/calculatorproject/CalculatorProject.java
+++ src/calculatorproject/CalculatorProject.java
@@ -1,12 +1,23 @@
 
 package calculatorproject;
 
+import java.awt.*;
 import javax.swing.*;
 public class CalculatorProject {
     
     JFrame frame = new JFrame("Calculator");
     
     CalculatorProject(){
+    
+    }
+    
+    public void prepareGui(){
+        frame.setSize(305,510);
+        frame.getContentPane().setLayout(null);
+        frame.getContentPane().setBackground(Color.black);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     
     }
 
